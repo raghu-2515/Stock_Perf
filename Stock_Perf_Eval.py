c3cdc5e1af05b387b2bf8daf006dad0ce2fdcbe3
raghu-2515 <123261139+raghu-2515@users.noreply.github.com>
2024-10-25 16:25:13 -0400

    Add files via upload

--- Stock_Perf_Eval.py
+++ Stock_Perf_Eval.py
@@ -48,8 +48,8 @@ if uploaded_file:
     hold_time=round((date.today()-datetime.date(purchase_date)).days/365,1)
 select_etf=st.sidebar.text_input("Enter ETF/Index Symbol to Compare",'SPY')
 
-Time_Period=("Hold Period","1y","3y","5y")
-Time_Frame=st.sidebar.selectbox("**Select Timeframe**",Time_Period,index=1)
+Time_Period=("Hold Period","1y","2y","5y")
+Time_Frame=st.sidebar.selectbox("**Select Timeframe**",Time_Period,index=0)
 Time_Interval="1d"
 
 if select_ticker:
